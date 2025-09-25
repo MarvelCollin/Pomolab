825c4f5671079e8348cd87b0e48e1eede4c7ef08
Wenfuuu <137779146+Wenfuuu@users.noreply.github.com>
2025-09-26 01:20:53 +0700

    fix: backend dockerfile to wait for db healthcheck

---
+++ backend/wait-for-db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# wait-for-db.sh
+
+set -e
+
+host="$1"
+port="$2"
+shift 2
+cmd="$@"
+
+until PGPASSWORD=$DB_PASSWORD psql -h "$host" -p "$port" -U "$DB_USERNAME" -d "$DB_DATABASE" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
