a072723d2eb88fb4a926bff5d21e6b1563e42bf2
Wenfuuu <137779146+Wenfuuu@users.noreply.github.com>
2026-01-01 09:42:47 +0700

    chore: optimize user migration

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password_hash')->nullable();
+            $table->string('role')->default('user');
+            $table->boolean('is_banned')->default(false);
             $table->timestamps();
         });
 
